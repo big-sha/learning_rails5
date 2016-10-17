d9441765f95ef424708f24d62bcbdfc9908ab0c9
perkins1 <smperk71@gmail.com>
2016-10-17 16:01:05 -0400

    add book data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+  description:%{<p><em>Native Apps, Multiple Platforms
+    </em>Answer the question “Can we build this for ALL the devices?” with aresounding YES. 
+    This book will help you get there with a real-worldintroduction to seven platforms, 
+    whether you’re new to mobile or anexperienced developer needing to expand your options. 
+    Plus, you’ll findout which cross-platform solution makes the most sense for your needs.</p>},
+    image_url: '7apps.jpg',price: 26.00)
